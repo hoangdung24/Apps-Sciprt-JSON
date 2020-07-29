2054a599509f441f5105d10cdcaf8c84a7400e57
Hoang Dung <conquynho.hd@gmail.com>
2020-07-29 11:57:04 +0700

    Fix code second time

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 function testDoGet() {
-    url = 'https://script.google.com/macros/s/AKfycbzWeixK8iukP7LNaokKZkCV2UsN7fLJQv_6nvL20Eud0Cu3N4w/exec';
+    url = 'https://script.google.com/macros/s/AKfycbyAAgutSyG9oGFbML0PWX-tn3dwva69Q3CLnVuM/exec';
 
     fetch(url)
         .then(resultJSON => resultJSON.json())
-        .then(result => document.querySelector('#app').textContent = result[0].status);
+        .then(result => document.querySelector('#app').textContent = result[0].data[0]['Tên Khách Hàng']);
 
 }
 
-function helloWorld() {
-    var element = document.createElement('h3');
-    element.textContent = 'Hello world';
-    document.querySelector('#app').appendChild(element);
-}
 
-document.querySelector('#btn').addEventListener('click', helloWorld);
+document.querySelector('#btn-1').addEventListener('click', testDoGet);
 
-document.querySelector('#btn').addEventListener('click', testDoGet);
+document.querySelector('#btn-2').addEventListener('click', testDoGet);
