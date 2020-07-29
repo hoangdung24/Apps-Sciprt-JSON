dbdea114dcfbcf30804c1957f66d92f2dbd4bd55
Hoang Dung <conquynho.hd@gmail.com>
2020-07-29 15:36:04 +0700

    finish a working today

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 function testDoGet() {
-    url = 'https://script.google.com/macros/s/AKfycbyAAgutSyG9oGFbML0PWX-tn3dwva69Q3CLnVuM/exec';
+    let url = 'https://script.google.com/macros/s/AKfycbyAAgutSyG9oGFbML0PWX-tn3dwva69Q3CLnVuM/exec';
 
     fetch(url)
         .then(resultJSON => resultJSON.json())
-        .then(result => document.querySelector('#app').textContent = result[0].data[0]['Tên Khách Hàng']);
+        .then(result => {
+            // result[0].data[0]['Tên Khách Hàng']
+            document.querySelector('#app').textContent = result[0].status;
+        });
+
+}
+
+function testDoPost() {
+    let url = 'https://script.google.com/macros/s/AKfycbyAAgutSyG9oGFbML0PWX-tn3dwva69Q3CLnVuM/exec';
+    let data = {
+        'Tên Khách Hàng' : 'Hoàng Dũng',
+        'Số Điện Thoại' : '0778639310',
+        'Địa Chỉ' : '544/12 Lạc Long Quân, Phường 5, Quận 11, TP.HCM'
+    };
+
+    fetch(url, {
+        method : 'POST',
+        mode : 'no-cors',
+        cache : 'no-cache',
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        redirect : 'follow',
+        body : JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+        document.querySelector('#app').textContent = result;
+    });
 
 }
 
 
 document.querySelector('#btn-1').addEventListener('click', testDoGet);
 
-document.querySelector('#btn-2').addEventListener('click', testDoGet);
+document.querySelector('#btn-2').addEventListener('click', testDoPost);
